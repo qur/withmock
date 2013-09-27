99f707fdf28047f5786ac80772e3dd881ed6ee25
Julian Phillips <julian@quantumfyre.co.uk>
2013-09-27 18:39:52 +0100

    Start keeping scenarios
    We want to build a collection of scenarios that we can use for
    regression testing.

---
+++ scenarios/basic/code.go
@@ -0,0 +1,22 @@
+package withdeps
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func Show(data string) error {
+	f := strings.NewReader(data)
+
+	s := bufio.NewScanner(f)
+	for i := 1; s.Scan(); i++ {
+		line := s.Text()
+		fmt.Printf("%d: %s\n", i, line)
+	}
+	if err := s.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
