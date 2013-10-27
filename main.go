4774c6faf26ec3feec5bf0f6055982252a120497
Julian Phillips <julian@quantumfyre.co.uk>
2013-10-27 21:14:28 +0000

    Fix copyright date.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright 2011 Julian Phillips.  All rights reserved.
+// Copyright 2013 Julian Phillips.  All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
