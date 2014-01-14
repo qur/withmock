4e7da74bc46803b4211558aed5b743ff9fef9e10
Julian Phillips <julian@quantumfyre.co.uk>
2014-01-14 20:41:24 +0000

    Expand issue #29 scenario to cover return values
    Since the parameters weren't working properly, the return values
    probably don't either - so expand the scenario to show that too.

--- scenarios/issue29/code_test.go
+++ scenarios/issue29/code_test.go
@@ -26,3 +26,20 @@ func TestAdd(t *testing.T) {
         t.Errorf("wrong result: %d\n", res)
     }
 }
+
+func TestSplitter(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	lib.MOCK().SetController(ctrl)
+
+	splitter := lib.MOCK().NewSplitter()
+	splitter.EXPECT().Split(5).Return(0, 0)
+
+	var s lib.Splitter = splitter
+
+	x, y := s.Split(5)
+	if x+y != 0 {
+		t.Errorf("expected mock splitter")
+	}
+}
--- scenarios/issue29/lib/lib.go
+++ scenarios/issue29/lib/lib.go
@@ -4,8 +4,18 @@ type Adder interface {
     Add(x, y int) int
 }
 
+type Splitter interface {
+	Split(a int) (x, y int)
+}
+
 type Foo struct {}
 
 func (c *Foo) Add(x, y int) int {
     return x+y
+}
+
+type Bar struct {}
+
+func (b *Bar) Split(a int) (x, y int) {
+	return a-1, 1
 }
