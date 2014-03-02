02b9f323c81128b3dd8030b077114be50e165ae1
Julian Phillips <julian@quantumfyre.co.uk>
2014-03-02 16:32:19 +0000

    Fix scenario for issue #32
    The test code was broken and didn't compile, when it should have.
    Apparently the test actually passes, so the issue was presumably fixed
    by the stdlib improvements that have been made.
    
    Fixes #32

--- scenarios/issue32/code_test.go
+++ scenarios/issue32/code_test.go
@@ -12,11 +12,11 @@ func TestMe(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	t := time.Time{}
+	t1 := time.Time{}
 
 	time.MOCK().SetController(ctrl)
-	time.EXPECT().Now().Return(t)
-	t.EXPECT().String().Return("wibble")
+	time.EXPECT().Now().Return(t1)
+	t1.EXPECT().String().Return("wibble")
 
 	if RunMe() != "Time: wibble" {
 		t.Errorf("expected mock time")
