3313937b7a2086a6a36b2aeb6b312f9352f1dda0
Julian Phillips <julian@quantumfyre.co.uk>
2014-12-30 02:13:51 +0000

    Some scenarios aren't valid with Go 1.4
    Since Go 1.4 no longer has support for C code, the scenarios that are
    testing C code are no longer valid.  However, I want to keep things
    working for older versions of Go - so deleting the scenarios isn't the
    appropriate response.

---
+++ scenarios/issue17b/lib_asm2/lib.go
@@ -0,0 +1,3 @@
+package lib_asm2
+
+func Wibble() bool
---
+++ scenarios/issue18b/code.go
@@ -0,0 +1,9 @@
+package code
+
+import (
+	"github.com/qur/withmock/scenarios/issue18b/lib1"
+)
+
+func TryMe() bool {
+	return lib1.Wibble()
+}
