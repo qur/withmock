82584bf6e896facd48feed7ff139335e2e09fcbe
Julian Phillips <julian@quantumfyre.co.uk>
2022-09-18 17:32:28 +0100

    Rework the way that the mock generator works
    Split the mock generator into two parts. First we collect information about the
    interface types that we want to create mocks for. Then we create the code in a
    second pass. The reason for doing this is so that we can add a middle stage -
    to correctly implement the mocks we need to resolve embedded interfaces so we
    can implement the methods of those too.

--- lib/codemod/mock.go
+++ lib/codemod/mock.go
@@ -23,6 +23,18 @@ import (
 	"github.com/qur/withmock/lib/proxy/modify"
 )
 
+type rawMock struct {
+	name    string
+	methods []*dst.Field
+}
+
+type rawMockFile struct {
+	path    string
+	pkgName string
+	imports []*dst.ImportSpec
+	mocks   []rawMock
+}
+
 type MockGenerator struct {
 	prefix    string
 	pkgFilter map[string]bool
@@ -60,7 +72,7 @@ func (i *MockGenerator) GenMod(ctx context.Context, mod, ver, src, dest string) 
 }
 
 func (i *MockGenerator) GenSource(ctx context.Context, mod, ver, zipfile, src, dest string) error {
-	totalOutput := 0
+	allMocks := []rawMockFile{}
 	origMod, err := i.stripPrefix(mod)
 	if err != nil {
 		return err
@@ -88,23 +100,83 @@ func (i *MockGenerator) GenSource(ctx context.Context, mod, ver, zipfile, src, d
 			return fmt.Errorf("failed to parse %s: %w", path, err)
 		}
 		for name, pkg := range pkgs {
-			output, err := i.processPackage(ctx, fset, origMod, path, src, dest, pkg)
+			mocks, err := i.processPackage(ctx, fset, origMod, path, src, dest, pkg)
 			if err != nil {
 				return fmt.Errorf("failed to process %s (%s): %w", path, name, err)
 			}
-			totalOutput += output
+			allMocks = append(allMocks, mocks...)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
-	if totalOutput == 0 {
+	if len(allMocks) == 0 {
 		return api.UnknownVersion(mod, ver)
 	}
-	return nil
+	return i.renderMocks(ctx, fset, allMocks)
 }
 
-func (i *MockGenerator) processPackage(ctx context.Context, fset *token.FileSet, mod, path, src, dest string, pkg *ast.Package) (int, error) {
+func (i *MockGenerator) processPackage(ctx context.Context, fset *token.FileSet, mod, path, src, dest string, pkg *ast.Package) ([]rawMockFile, error) {
+	files := []rawMockFile{}
+	rel, err := filepath.Rel(src, path)
+	if err != nil {
+		return files, err
+	}
+	pkgPath := filepath.Join(mod, rel)
+	for path, f := range pkg.Files {
+		in, err := decorator.DecorateFile(fset, f)
+		if err != nil {
+			return files, err
+		}
+		mocks := []rawMock{}
+		log.Printf("PROCESS: %s %s", path, pkgPath)
+		for _, node := range in.Decls {
+			if err := ctx.Err(); err != nil {
+				// request cancelled, give up
+				return files, err
+			}
+			n, ok := node.(*dst.GenDecl)
+			if !ok || n.Tok != token.TYPE {
+				// not a type decl
+				continue
+			}
+			for _, spec := range n.Specs {
+				t := spec.(*dst.TypeSpec)
+				ift, ok := t.Type.(*dst.InterfaceType)
+				if !t.Name.IsExported() || !ok || ift.Methods == nil || len(ift.Methods.List) == 0 {
+					continue
+				}
+				methods := []*dst.Field{}
+				log.Printf("TYPE: %s %#v", t.Name.Name, ift)
+				for _, f := range ift.Methods.List {
+					log.Printf("METHOD: %s %T", f.Names, f.Type)
+					methods = append(methods, dst.Clone(f).(*dst.Field))
+				}
+				mocks = append(mocks, rawMock{
+					name:    "Mock" + t.Name.Name,
+					methods: methods,
+				})
+			}
+		}
+		if len(mocks) == 0 {
+			// no mocks were added, so skip the whole file
+			continue
+		}
+		rel, err := filepath.Rel(src, path)
+		if err != nil {
+			return files, fmt.Errorf("failed to resolve extra path %s: %s", path, err)
+		}
+		files = append(files, rawMockFile{
+			path:    filepath.Join(dest, rel),
+			pkgName: in.Name.Name,
+			imports: in.Imports,
+			mocks:   mocks,
+		})
+	}
+	return files, nil
+}
+
+func (i *MockGenerator) processPackageOld(ctx context.Context, fset *token.FileSet, mod, path, src, dest string, pkg *ast.Package) (int, error) {
 	output := 0
 	rel, err := filepath.Rel(src, path)
 	if err != nil {
@@ -246,4 +318,90 @@ func (*MockGenerator) save(dest string, fset *token.FileSet, node *dst.File) err
 	}
 	defer f.Close()
 	return decorator.Fprint(f, node)
+}
+
+func (i *MockGenerator) renderMocks(ctx context.Context, fset *token.FileSet, files []rawMockFile) error {
+	for _, file := range files {
+		if err := ctx.Err(); err != nil {
+			// request cancelled, give up
+			return err
+		}
+		out := &dst.File{
+			Name: dst.NewIdent(file.pkgName),
+		}
+		imports := &dst.GenDecl{
+			Tok: token.IMPORT,
+		}
+		for _, imp := range file.imports {
+			imports.Specs = append(imports.Specs, dst.Clone(imp).(*dst.ImportSpec))
+		}
+		out.Decls = append(out.Decls, imports, &dst.GenDecl{
+			Tok: token.IMPORT,
+			Specs: []dst.Spec{
+				&dst.ImportSpec{
+					Path: &dst.BasicLit{
+						Kind:  token.STRING,
+						Value: `"gowm.in/ctrl/mock"`,
+					},
+				},
+			},
+			Decs: dst.GenDeclDecorations{
+				NodeDecs: dst.NodeDecs{
+					Before: dst.EmptyLine,
+					After:  dst.EmptyLine,
+				},
+			},
+		})
+		for _, mock := range file.mocks {
+			out.Decls = append(out.Decls, &dst.GenDecl{
+				Tok: token.TYPE,
+				Specs: []dst.Spec{
+					&dst.TypeSpec{
+						Name: dst.NewIdent(mock.name),
+						Type: &dst.StructType{
+							Fields: &dst.FieldList{
+								List: []*dst.Field{
+									{
+										Type: &dst.SelectorExpr{
+											X:   dst.NewIdent("mock"),
+											Sel: dst.NewIdent("Mock"),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Decs: dst.GenDeclDecorations{
+					NodeDecs: dst.NodeDecs{
+						After: dst.EmptyLine,
+					},
+				},
+			})
+			for _, method := range mock.methods {
+				if ft, ok := method.Type.(*dst.FuncType); ok {
+					out.Decls = append(out.Decls, &dst.FuncDecl{
+						Recv: &dst.FieldList{
+							List: []*dst.Field{
+								{
+									Names: []*dst.Ident{
+										dst.NewIdent("m"),
+									},
+									Type: &dst.StarExpr{
+										X: dst.NewIdent(mock.name),
+									},
+								},
+							},
+						},
+						Name: dst.NewIdent(method.Names[0].Name),
+						Type: ft,
+					})
+				}
+			}
+		}
+		if err := i.save(file.path, fset, out); err != nil {
+			return fmt.Errorf("failed to format %s: %w", file.path, err)
+		}
+	}
+	return nil
 }
